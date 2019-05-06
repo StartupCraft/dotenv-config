6c971884c2053d3f3c399c5f29cc41b7637f903b
George Viazemsky <george@startupcraft.io>
2019-05-06 13:46:56 +0300

    Initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import '../src'
+
+describe('Mergers', () => {
+  test('Test should be false', () => {
+    expect(process.env.TEST).toEqual('false')
+  })
+})
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import { config } from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+
+const { NODE_ENV } = process.env
+
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.',
+  )
+}
+
+const dotenvFiles = [
+  `.env.${NODE_ENV}.local`,
+  `.env.${NODE_ENV}`,
+  NODE_ENV !== 'test' && `.env.local`,
+  '.env',
+].filter(Boolean)
+
+dotenvFiles.forEach(dotenvFile => {
+  if (fs.existsSync(dotenvFile)) {
+    dotenvExpand(
+      config({
+        path: dotenvFile,
+      }),
+    )
+  }
+})
